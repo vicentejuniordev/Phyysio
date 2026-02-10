b0e3daa6ef1c27bc1cfff77aa1f749bbd4a8a580
vicentejuniordev <vicentejunior4522@gmail.com>
2026-02-10 13:38:01 -0300

    feat: add header in home landing page

---
+++ phyysio-frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/phyysio-logo-.png';
+import './Header.css';
+function Header(){
+    return (
+        <>
+          <div className="header">
+              <img className="logo" src={logo} alt="Phyysio Logo" />
+           <div className='header-words'>
+             <p className='word'>Home</p>
+             <p className='word' style={{color:'#cb6ce6'}}>Sobre Nós</p>
+             <p className='word'>Serviços</p>
+             <p className='word' style={{color:'#cb6ce6'}}>Contato</p>
+           </div>
+          </div>
+        </>
+    );
+}
+
+export default Header;
--- phyysio-frontend/src/pages/Home/Home.jsx
+++ phyysio-frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import './Home.css';
-import Header from '../../components/Header';
+import Header from '../../components/Header/Header';
 function Home() {
   return (
     <>
