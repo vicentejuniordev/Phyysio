17c6bc13c15cf493f189d7ed0f20bc5feafe424d
vicentejuniordev <vicentejunior4522@gmail.com>
2026-02-27 10:07:58 -0300

    fix: change some colors

--- phyysio-frontend/src/components/Text-Home/TextHome.jsx
+++ phyysio-frontend/src/components/Text-Home/TextHome.jsx
@@ -6,20 +6,21 @@ function TextHome() {
     <>
       <div className="text-home-container">
         <div className="text-home">
-        <h1 className="title">Bem-vindo à Ph<span className="highlight">yy</span>sio</h1>
-        <p className="text-simple">
-          A rotina de uma clínica de fisioterapia exige organização, atenção aos
-          pacientes e controle de informações. O Phyysio foi criado para
-          substituir processos manuais e papéis por um sistema simples,
-          confiável e fácil de usar.
-        </p>
-      </div>
-      <img src={image} className="home-image" alt="Imagem de boas-vindas" />
+          <h1 className="title">
+            Bem-vindo à Ph<span className="highlight">yy</span>sio
+          </h1>
+          <p className="text-simple">
+            A rotina de uma clínica de fisioterapia exige organização, atenção
+            aos pacientes e controle de informações. O Phyysio foi criado para
+            substituir processos manuais e papéis por um sistema simples,
+            confiável e fácil de usar.
+          </p>
+          <button className="primary-btn">Conhecer o sistema</button>
+        </div>
+        <img src={image} className="home-image" alt="Imagem de boas-vindas" />
       </div>
     </>
   );
 }
 
 export default TextHome;
-
-
