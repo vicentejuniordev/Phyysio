4b5da976db7c5adb32b18b2a1be06eb000df32ea
vicentejuniordev <vicentejunior4522@gmail.com>
2026-02-28 15:12:35 -0300

    fix: fix mobile responsiveness header component

--- phyysio-frontend/src/components/Header/Header.jsx
+++ phyysio-frontend/src/components/Header/Header.jsx
@@ -1,19 +1,36 @@
-import logo from '../../assets/img/phyysio-logo-.png';
-import './Header.css';
-function Header(){
-    return (
-        <>
-          <div className="header">
-              <img className="logo" src={logo} alt="Phyysio Logo" />
-           <div className='header-words'>
-             <a className='word'href="#text-home-container"><p >Início</p></a>
-             <a className='word' href="#services-home-container"><p style={{color:'#cb6ce6'}}>Serviços</p></a>
-             <a className='word' href="#benefits-section"><p>Beneficios</p></a>
-             <a className='word' href="#contact-section"><p style={{color:'#cb6ce6'}}>Contato</p></a>
-           </div>
+import logo from "../../assets/img/phyysio-logo-2.png";
+import "./Header.css";
+import menuImage from "../../assets/img/menu.png";
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <img className="logo" src={logo} alt="Phyysio Logo" />
+        <div className="menu-icon">
+          <img src={menuImage} alt="Menu" />
+        </div>
+        <div className="right-side">
+          <div className="header-words">
+            <a className="word" href="#text-home-container">
+              <p>Início</p>
+            </a>
+            <a className="word" href="#services-home-container">
+              <p style={{ color: "#cb6ce6" }}>Serviços</p>
+            </a>
+            <a className="word" href="#benefits-section">
+              <p>Beneficios</p>
+            </a>
+            <a className="word" href="#contact-section">
+              <p style={{ color: "#cb6ce6" }}>Contato</p>
+            </a>
+            <div className="menu-icon">
+              <img src={menuImage} alt="Menu" />
+            </div>
           </div>
-        </>
-    );
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Header;
