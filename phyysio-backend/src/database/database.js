4a6025dede4ec8db5f9a3b63fcbc74fc27fc88aa
vicentejuniordev <vicentejunior4522@gmail.com>
2026-03-10 15:21:19 -0300

    feat: add config database setup

---
+++ phyysio-backend/src/database/database.js
@@ -0,0 +1,19 @@
+
+import { Pool } from 'pg';
+
+
+console.log(process.env.POSTGRES_USER);
+
+async function getClient(){
+    const pool = new Pool({
+        user: process.env.POSTGRES_USER,
+        host: 'localhost',
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+        port: 5432,
+    });
+    return pool;
+}
+
+export default getClient;
+
