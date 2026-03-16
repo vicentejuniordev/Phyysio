fbea628b7e2f041b122d50adff7b29fd65d8c42d
vicentejuniordev <vicentejunior4522@gmail.com>
2026-03-16 13:16:33 -0300

    fix: add CPF field in `createUser` function

--- phyysio-backend/src/models/user/user-model.js
+++ phyysio-backend/src/models/user/user-model.js
@@ -3,8 +3,8 @@ import getPool from "../../database/database.js";
 async function createUser(userData) {
     try {
         const pool = await getPool();
-        const query = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *';
-        const values = [userData.name, userData.email, userData.password];
+        const query = 'INSERT INTO users (cpf, name, email, password) VALUES ($1, $2, $3, $4) RETURNING *';
+        const values = [userData.cpf, userData.name, userData.email, userData.password];
         const result = await pool.query(query, values);
         return result.rows[0];
     } catch (error) {
@@ -13,6 +13,9 @@ async function createUser(userData) {
     }
 }
 
+
+
 export default {
     createUser,
+    getUserByEmail,
 };
